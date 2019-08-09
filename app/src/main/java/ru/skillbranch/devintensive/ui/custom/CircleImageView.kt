5c90221c10629280261fb826857424a5e7aeab55
Olga <olga.kohanova@gmail.com>
2019-08-09 11:49:50 +0300

    lesson 4 edit circle view

--- app/src/main/java/ru/skillbranch/devintensive/ui/custom/CircleImageView.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/custom/CircleImageView.kt
@@ -13,10 +13,9 @@ class CircleImageView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : ImageView(context, attrs, defStyleAttr)
-{
-    companion object{
-        private const val DEFAULT_BORDER_COLOR = Color.WHITE
+) : ImageView(context, attrs, defStyleAttr) {
+    companion object {
+        private const val DEFAULT_BORDER_COLOR: Int = Color.WHITE
         private const val DEFAULT_BORDER_WIDTH = 2f
     }
 
@@ -25,7 +24,7 @@ class CircleImageView @JvmOverloads constructor(
 
 
     init {
-        if(attrs != null){
+        if (attrs != null) {
             val a = context.obtainStyledAttributes(attrs, R.styleable.CircleImageView)
             borderColor = a.getColor(R.styleable.CircleImageView_cv_borderColor, DEFAULT_BORDER_COLOR)
             borderWidth = a.getDimension(R.styleable.CircleImageView_cv_borderWidth, DEFAULT_BORDER_WIDTH)
@@ -39,17 +38,27 @@ class CircleImageView @JvmOverloads constructor(
         paint.strokeWidth = borderWidth
         paint.setColor(borderColor)
         paint.style = Paint.Style.STROKE
-        canvas?.drawCircle(width/2f, height/2f, height/2f, paint)
+        canvas?.drawCircle(width / 2f, height / 2f, height / 2f, paint)
     }
-/*    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
 
-        setBorderWidth(borderWidth)
+    private fun getBorderWidth(): Int {
+        return borderWidth.toInt()
+    }
 
-        setBorderColor(borderColor)
+    private fun setBorderWidth(dp: Int) {
+        borderWidth = dp.toFloat()
+    }
 
+    private fun getBorderColor():Int{
+        return borderColor
+    }
 
-    }*/
+    private fun setBorderColor(hex:String){
+        borderColor = Integer.decode(hex)
+    }
 
+    private fun setBorderColor(colorId: Int){
+        borderColor = colorId
+    }
 
 }
